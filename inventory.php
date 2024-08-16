6603cb50ec9acfcd363114e9a9752a3e1362b095
devyqt <130561047+devyqt@users.noreply.github.com>
2024-08-16 16:51:32 +0800

    inventory.php

--- inventory.php
+++ inventory.php
@@ -27,22 +27,22 @@
         <div id="cylinders" class="tab-pane active">
           <div class="product-container">
             <div class="box">
-              <img src="images/11kg.jpg" alt="Product 1" class="product-img" style="width: 250px; height: 250px; object-fit: cover;">
+              <img src="images/11kg.jpg" alt="Product 1" class="product-img" style="width: 230px; height: 230px; object-fit: cover;">
               <p><strong>Product Name:</strong> 11kg Auto-Shutoff Cylinder</p>
               <p><strong>Description:</strong> High quality cylinder.</p>
             </div>
             <div class="box">
-              <img src="images/14kg.jpg" alt="Product 2" class="product-img" style="width: 250px; height: 250px; object-fit: cover;">
+              <img src="images/14kg.jpg" alt="Product 2" class="product-img" style="width: 230px; height: 230px; object-fit: cover;">
               <p><strong>Product Name:</strong> 1.4kg Solane Sakto</p>
               <p><strong>Description:</strong> Durable and reliable.</p>
             </div>
             <div class="box">
-              <img src="images/22kg.jpg" alt="Product 3" class="product-img" style="width: 250px; height: 250px; object-fit: cover;">
+              <img src="images/22kg.jpg" alt="Product 3" class="product-img" style="width: 230px; height: 230px; object-fit: cover;">
               <p><strong>Product Name:</strong> 22kg POL Cylinder</p>
               <p><strong>Description:</strong> High quality cylinder.</p>
             </div>
             <div class="box">
-              <img src="images/50kg.jpg" alt="Product 4" class="product-img" style="width: 250px; height: 250px; object-fit: cover;">
+              <img src="images/50kg.jpg" alt="Product 4" class="product-img" style="width: 230px; height: 230px; object-fit: cover;">
               <p><strong>Product Name:</strong> 50kg Cylinder</p>
               <p><strong>Description:</strong> Durable and reliable.</p>
             </div>
